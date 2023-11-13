3d77655190b2786694406272689667b146173e10
miilord <30593788+miilord@users.noreply.github.com>
2023-11-13 20:10:48 +0800

    test: refactor unit tests for DoTransaction

--- transaction_test.go
+++ transaction_test.go
@@ -16,32 +16,32 @@ type TestCounter struct {
 	Count        int    `bson:"count,omitempty" json:"count"`
 }
 
-type TestPapers struct {
+type TestPaper struct {
 	DefaultField `bson:",inline"`
 	Text         string `bson:"text,omitempty" json:"text"`
 	NumberID     int    `bson:"number_id,omitempty" json:"numberId"`
 }
 
-func (u *TestPapers) Uniques() []string {
+func (u *TestPaper) Uniques() []string {
 	return []string{"number_id"}
 }
 
 type TestDB struct {
 	DoTransaction DoTransactionFunc
-	Papers        *Repo[*TestPapers]
+	Papers        *Repo[*TestPaper]
 	Counters      *Repo[*TestCounter]
 }
 
-func TestTransaction(t *testing.T) {
+func TestDoTransaction(t *testing.T) {
 	database, cleanup := setupTestDatabase(t)
 	defer cleanup()
 	db := TestDB{
-		Papers:   NewRepo[*TestPapers](database.Collection("test_papers")),
+		Papers:   NewRepo[*TestPaper](database.Collection("test_papers")),
 		Counters: NewRepo[*TestCounter](database.Collection("test_counters")),
 	}
 	db.DoTransaction = DoTransaction(database.Client())
 	ctx := context.TODO()
-	err := db.Papers.EnsureIndexesByModel(ctx, &TestPapers{})
+	err := db.Papers.EnsureIndexesByModel(ctx, &TestPaper{})
 	require.NoError(t, err)
 
 	_, err = db.Counters.InsertOne(ctx, &TestCounter{Key: "paper", Count: 0})
@@ -57,11 +57,11 @@ func TestTransaction(t *testing.T) {
 			return nil, err
 		}
 
-		paper, err := db.Papers.InsertOne(sessCtx, &TestPapers{Text: "go", NumberID: paperCounter.Count})
+		paper, err := db.Papers.InsertOne(sessCtx, &TestPaper{Text: "go", NumberID: paperCounter.Count})
 		return paper, err
 	})
 	require.NoError(t, err)
-	paper, ok := result.(*TestPapers)
+	paper, ok := result.(*TestPaper)
 	require.True(t, ok)
 	assert.Equal(t, 1, paper.NumberID)
 
@@ -75,14 +75,39 @@ func TestTransaction(t *testing.T) {
 			return nil, err
 		}
 
-		paper, err := db.Papers.InsertOne(sessCtx, &TestPapers{Text: "go", NumberID: paperCounter.Count})
+		paper, err := db.Papers.InsertOne(sessCtx, &TestPaper{Text: "go", NumberID: paperCounter.Count})
 		return paper, err
 	})
 	require.NoError(t, err)
-	paper2, ok := result2.(*TestPapers)
+	paper2, ok := result2.(*TestPaper)
 	require.True(t, ok)
 	assert.Equal(t, 2, paper2.NumberID)
 
-	_, err = db.Papers.InsertOne(ctx, &TestPapers{Text: "go", NumberID: 2})
+	_, err = db.Papers.InsertOne(ctx, &TestPaper{Text: "go", NumberID: 2})
 	require.True(t, mongo.IsDuplicateKeyError(err))
+
+	t.Run("Error Case - StartSession", func(t *testing.T) {
+		mockClient := &mongo.Client{}
+		// Mock callback function
+		mockCallback := func(sessCtx context.Context) (interface{}, error) {
+			return nil, nil
+		}
+
+		result, err := DoTransaction(mockClient)(context.Background(), mockCallback)
+		assert.Error(t, err)
+		assert.Nil(t, result)
+	})
+
+	t.Run("Error Case - WithTransaction", func(t *testing.T) {
+		ctx := context.TODO()
+		result, err := db.DoTransaction(ctx, func(sessCtx context.Context) (interface{}, error) {
+			paper, err := db.Papers.FindOne(
+				sessCtx,
+				&TestPaper{NumberID: 404},
+			)
+			return paper, err
+		})
+		require.Error(t, err)
+		assert.Nil(t, result)
+	})
 }
